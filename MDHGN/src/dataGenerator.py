e76af8486b6448a924b85c2b619065f36378eb80
wonjongRyu <wonjong4773@gmail.com>
2020-09-04 16:16:32 +0900

    Update dataGenerator.py

--- MDHGN/src/dataGenerator.py
+++ MDHGN/src/dataGenerator.py
@@ -9,64 +9,60 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
-def drawDot(N, rad):
-    Img = np.zeros((512, 512))
-    for _ in range(N):
-        dotx = np.random.randint(0, 512)
-        doty = np.random.randint(0, 512)
-        Img = cv2.circle(Img, (dotx, doty), rad, (255, 255, 255), -1)
-    return Img
+class dataGenerator():
+    def __init__(self, imageSize, pixelSize, propDistance):
+        self.Nx = imageSize
+        self.Ny = imageSize
+        self.dx = pixelSize*um
+        self.Wav = 532*nm
+        self.Sizex = self.Nx*self.dx
+        self.Sizey = self.Ny*self.dx
+        self.z = propDistance*cm  # 전파거리
 
+    def drawDot(self, dotNum, radius):
+        Img = np.zeros((512, 512))
+        for _ in range(dotNum):
+            dotx = np.random.randint(0, 512)
+            doty = np.random.randint(0, 512)
+            Img = cv2.circle(Img, (dotx, doty), radius, (255, 255, 255), -1)
+        return Img
 
-def drawHologram(Img):
-    Nx = 512
-    Ny = 512
-    Dx = 3.6*um
-    Wav = 532*nm
-    Sizex = Nx*Dx
-    Sizey = Ny*Dx
-    PropDis = 5*cm  # 전파거리
+    def drawHologram(self, Img):
 
-    Source = Begin(Sizey, Wav, Ny)
+        Source = Begin(self.Sizey, self.Wav, self.Ny)
 
-    Source.field = Source.field * Img
+        Source.field = Source.field * Img
 
-    ComplexHologram = Forvard(Source, PropDis)
+        ComplexHologram = Forvard(Source, self.z)
 
-    PhaseHologram = np.angle(ComplexHologram.field)
-    AmplitudeHologram = np.abs(ComplexHologram.field)
+        PhaseHologram = np.angle(ComplexHologram.field)
+        AmplitudeHologram = np.abs(ComplexHologram.field)
 
-    RealHologram = np.real(ComplexHologram.field)
-    ImaginaryHologram = np.imag(ComplexHologram.field)
+        RealHologram = np.real(ComplexHologram.field)
+        ImaginaryHologram = np.imag(ComplexHologram.field)
 
-    return AmplitudeHologram, PhaseHologram, RealHologram, ImaginaryHologram
+        return AmplitudeHologram, PhaseHologram, RealHologram, ImaginaryHologram
 
+    def makeTrainData(self, path, num_of_data):
+        count = 0
+        for num in range(num_of_data//100):
+            for dotnum in range(10, 20, 1):  # 찍을점개수
+                for radius in range(0, 10, 1):  # 점크기
+                    count += 1
+                    Source = self.drawDot(dotnum, radius)
+                    [AH, PH, RH, IH] = self.drawHologram(Source)
+                    self.saveAsImage(path, Source, RH, IH, count)
 
-def makeTrainData(sz, path, num_of_data):
-    count = 0
-    for num in range(num_of_data//100):
-        for dotnum in range(10, 20, 1):  # 찍을점개수
-            for rad in range(0, 10, 1):  # 점크기
-                count += 1
-                Source = drawDot(dotnum, rad)
-                [AH, PH, RH, IH] = drawHologram(Source)
+    def makeTestData(self, path, num_of_data):
+        for idx in range(1, num_of_data+1):
+            dotnum = np.random.randint(10, 20)  # 찍을점개수
+            radius = np.random.randint(0, 10)  # 점크기
+            Source = self.drawDot(dotnum, radius)
+            [AH, PH, RH, IH] = self.drawHologram(Source)
 
-                savePath_of_Source = os.path.join(
-                    path, 'Source', str(count)+'.png')
-                savePath_of_RH = os.path.join(path, 'RH', str(count)+'.png')
-                savePath_of_IH = os.path.join(path, 'IH', str(count)+'.png')
-                plt.imsave(savePath_of_Source, Source, cmap='gray')
-                plt.imsave(savePath_of_RH, RH, cmap='gray')
-                plt.imsave(savePath_of_IH, IH, cmap='gray')
+            self.saveAsImage(path, Source, RH, IH, idx)
 
-
-def makeTestData(sz, path, num_of_data):
-    for idx in range(1, num_of_data+1):
-        dotnum = np.random.randint(10, 20)  # 찍을점개수
-        rad = np.random.randint(0, 10)  # 점크기
-        Source = drawDot(dotnum, rad)
-        [AH, PH, RH, IH] = drawHologram(Source)
-
+    def saveAsImage(self, path, Source, RH, IH, idx):
         savePath_of_Source = os.path.join(path, 'Source', str(idx)+'.png')
         savePath_of_RH = os.path.join(path, 'RH', str(idx)+'.png')
         savePath_of_IH = os.path.join(path, 'IH', str(idx)+'.png')
@@ -76,10 +72,16 @@ def makeTestData(sz, path, num_of_data):
 
 
 def main():
-    sz = 512
-    path_of_dataset = "../dataset"
+    imageSize = 512
+    pixelSize = 3.6  # um
+    propDistance = 1  # cm
     num_of_trainData = 1000  # valid = train/10, test= valid/10
     num_of_testData = 20
+
+    dG = dataGenerator(imageSize, pixelSize, propDistance)
+
+    path_of_dataset = "../dataset/" + \
+        str(pixelSize)+'um'+str(propDistance)+'cm'
 
     for phase in ['train', 'valid', 'test']:
         for folder in ['Source', 'RH', 'IH']:
@@ -87,9 +89,9 @@ def main():
             if not os.path.exists(path):
                 os.makedirs(path)
 
-    makeTrainData(sz, path_of_dataset+"/train", num_of_trainData)
-    makeTrainData(sz, path_of_dataset+"/valid", num_of_trainData//10)
-    makeTestData(sz, path_of_dataset+"/test", num_of_testData)
+    dG.makeTrainData(path_of_dataset+"/train", num_of_trainData)
+    dG.makeTrainData(path_of_dataset+"/valid", num_of_trainData//10)
+    dG.makeTestData(path_of_dataset+"/test", num_of_testData)
 
 
 if __name__ == "__main__":
